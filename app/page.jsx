7bae1808bd388e2b86dc1df81dfd0d2eec1bd2bd
Sudhanshu <lohanasudhanshu8257@gmail.com>
2024-09-01 19:26:53 +0530

    added git link

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,18 @@
 import PriceAnanlysis from "@/components/PriceAnalysis";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 lg:gap-8 p-4 lg:p-12">
-      <h1 className=" text-xl lg:text-3xl font-bold mb-6">
-        Commodity Price Analysis
-      </h1>
+      <div className="w-full flex items-center mb-6 justify-between">
+        <h1 className=" text-xl lg:text-3xl font-bold ">
+          Commodity Price Analysis
+        </h1>
+        <Link href="https://github.com/Sudhanshu8257/orders-chart/" >
+          <GitHubLogoIcon width={24} height={24} />
+        </Link>
+      </div>
       <PriceAnanlysis />
     </main>
   );
